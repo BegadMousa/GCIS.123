7413635493bace3f0849ad963fed4fd56386772a
begadwael <begad.mestar@gmail.com>
2022-09-07 17:28:16 +0400

    Prompt and print

---
+++ prompt_and_print.py
@@ -0,0 +1,12 @@
+def prompt_and_print():
+    num1 = int(input("Enter An Number: "))
+    num2 = int(input("Enter Another Number: "))
+
+
+    print(f"{num1} + {num2} = {num1 - num2}")
+    print(f"{num1} - {num2} = {num1 - num2}")
+    print(f"{num1} * {num2} = {num1 * num2}")
+    print(f"{num1} / {num2} = {num1 / num2}")
+
+if __name__ == "__main__":
+    prompt_and_print()
