f06179b7a623a5fa9d67d8a30ebb43433763ca3b
begadwael <begad.mestar@gmail.com>
2022-09-07 09:32:23 +0400

    happy Bday

---
+++ h_birthday.py
@@ -0,0 +1,7 @@
+name = input("What is your name?: ")
+month = input("What month were you born in?: ")
+day = input("What day were you born on?: ")
+year = input("What year were you born in?: ")
+
+
+print(f'Hello {name}! your birthday is on {day},{month},{year}!')  
